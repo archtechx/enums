10b68d7b399f1071eb8b8803d15f32c46eba6846
Eric Junker <eric.junker@dice.com>
2022-02-20 20:45:55 -0600

    use array_column() to get the names, values, and options

--- src/Values.php
+++ src/Values.php
@@ -9,8 +9,6 @@ trait Values
     /** Get an array of case values. */
     public static function values(): array
     {
-        return array_map(function ($case) {
-            return $case->value;
-        }, static::cases());
+        return array_column(static::cases(), 'value');
     }
 }
